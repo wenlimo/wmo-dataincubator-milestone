936c0e3a881bf8f2b7f289a041948d55582afd63
Wenli Mo <wenli.mo@gmail.com>
2018-09-09 16:27:56 -0400

    commit

--- wrapper.py
+++ wrapper.py
@@ -34,4 +34,5 @@ def process():
 #        return 'Please go back and enter your name...', 400  # 400 Bad Request
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
